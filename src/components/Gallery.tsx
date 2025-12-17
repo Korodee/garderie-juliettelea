2effca5042a86ef9e37b7aef7d4fb3588997641d
Korodee <koroskki@gmail.com>
2025-12-17 09:10:59 +0100

    chore: fix lint in gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { FaCamera, FaBuilding, FaChild, FaTree, FaDoorOpen, FaStar, FaTimes } from "react-icons/fa";
+import { FaCamera, FaBuilding, FaChild, FaDoorOpen, FaStar, FaTimes } from "react-icons/fa";
 import Image from "next/image";
 import { useState } from "react";
 import { useLanguage } from "@/contexts/LanguageContext";
